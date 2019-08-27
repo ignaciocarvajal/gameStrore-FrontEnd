7662cf8b63af6a064599b76caaee43525c6f7901
Neysa <nalarcon@everis.com>
2019-08-27 17:57:25 -0400

    Added: routing for authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './shared/components/login/login.component';
-
+import { ListGamesComponent } from './shared/components/games/list-games/list-games.component';
+import { RegisterClientComponent } from './shared/components/login/register-client/register-client.component';
+import { ChangePasswordComponent } from './shared/components/login/change-password/change-password.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: ListGamesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterClientComponent },
+  { path: 'change-password', component: ChangePasswordComponent }
 ];
 
 @NgModule({
