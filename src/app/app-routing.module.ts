4432906c40db1b75be9d5df330009199ee90b8eb
Neysa <nalarcon@everis.com>
2019-08-30 16:34:11 -0400

    Guard: added restriction of access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './shared/components/login/login.component';
 import { ListGamesComponent } from './shared/components/games/list-games/list-games.component';
 import { RegisterClientComponent } from './shared/components/login/register-client/register-client.component';
 import { ChangePasswordComponent } from './shared/components/login/change-password/change-password.component';
 import { GamesComponent } from './shared/components/games/games.component';
 import { ClientsComponent } from './shared/components/clients/clients.component';
+import { AuthGuard } from './model/objects/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: ListGamesComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterClientComponent },
   { path: 'change-password', component: ChangePasswordComponent },
-  { path: 'games', component: GamesComponent },
-  { path: 'clients', component: ClientsComponent },
+  { path: 'games', component: GamesComponent, canActivate: [AuthGuard] },
+  { path: 'clients', component: ClientsComponent, canActivate: [AuthGuard] },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
